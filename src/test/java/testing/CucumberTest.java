36e4a921e03aa92e5275cd8571f13ddaa0a42373
Revaldo Dhamacora <revaldodhamacora22@gmail.com>
2025-06-22 15:18:41 +0700

    commit update with cucumber reports

---
+++ src/test/java/testing/CucumberTest.java
@@ -0,0 +1,17 @@
+package testing;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"com.valdo"},
+        features = {"src/test/resources"},
+        plugin = {"pretty","html:target/cucumber-reports.html"},
+        tags = "not @exclude"
+)
+
+public class CucumberTest {
+
+}
